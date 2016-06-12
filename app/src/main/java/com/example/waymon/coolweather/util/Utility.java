4e85862f050a9eafbf8ead76d04cf43d232d293e
waymon <527606652@qq.com>
2016-06-12 22:24:12 +0800

    接口不能用 先不做了

--- app/src/main/java/com/example/waymon/coolweather/util/Utility.java
+++ app/src/main/java/com/example/waymon/coolweather/util/Utility.java
@@ -1,7 +1,18 @@
 package com.example.waymon.coolweather.util;
 
+import android.text.TextUtils;
+
+import com.example.waymon.coolweather.db.CoolWeatherDB;
+
 /**
  * Created by Administrator on 2016/6/12 0012.
  */
 public class Utility {
+    public synchronized static boolean handleProvincesResponse(
+            CoolWeatherDB coolWeatherDB, String response){
+        if(!TextUtils.isEmpty(response)){
+        }
+
+        return true;
+    }
 }
